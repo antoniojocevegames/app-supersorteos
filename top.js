deed6d3877c25c897ce90b8ae4d90045bfce5f0f
antoniojoceve <antoniofebrero24@hotmail.com>
2026-01-13 22:07:52 +0100

    fix top patch

--- top.js
+++ top.js
@@ -1,29 +1,21 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzLz5X6sz-7v41YK-aNnl1mnBrZbUUM121uqFNNWjOPpqV09L_r_EropNCEYhdULzKplw/exec?action=top";
+function renderTop(data) {
+  const body = document.getElementById("topBody");
+  body.innerHTML = "";
 
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(data => {
-        const body = document.getElementById("topBody");
-        body.innerHTML = "";
+  if (!data || !data.length) {
+    body.innerHTML = "<tr><td colspan='3'>Sin datos</td></tr>";
+    return;
+  }
 
-        if (!data.length) {
-          body.innerHTML = "<tr><td colspan='3'>Sin datos</td></tr>";
-          return;
-        }
-
-        data
-          .sort((a, b) => b.tickets - a.tickets)
-          .forEach((u, i) => {
-            const tr = document.createElement("tr");
-            tr.innerHTML = `
-              <td class="rank">${i + 1}</td>
-              <td>${u.nombre}</td>
-              <td>${u.tickets}</td>
-            `;
-            body.appendChild(tr);
-          });
-      })
-      .catch(() => {
-        document.getElementById("topBody").innerHTML =
-          "<tr><td colspan='3'>Error cargando datos</td></tr>";
-      });
+  data
+    .sort((a, b) => b.tickets - a.tickets)
+    .forEach((u, i) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td class="rank">${i + 1}</td>
+        <td>${u.nombre}</td>
+        <td>${u.tickets}</td>
+      `;
+      body.appendChild(tr);
+    });
+}
